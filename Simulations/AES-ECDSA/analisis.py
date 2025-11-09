5b95b389b4eb6bbe0077dc5fda908b955bb060ea
MigueldeHaroce <migueldeharoce@gmail.com>
2025-11-09 23:02:27 +0100

    Simulation AES-ECDSA

--- Simulations/AES-ECDSA/analisis.py
+++ Simulations/AES-ECDSA/analisis.py
@@ -2,28 +2,24 @@ import pandas as pd
 
 df = pd.read_csv("resultados.csv")
 
-stats = df.melt(id_vars=["Tipo de dato", "Archivo", "Tamaño (bytes)", "Repetición"],
-                value_vars=["AES-256 tiempo (s)", "ECDSA tiempo (s)"],
-                var_name="Protocolo", value_name="Tiempo (s)")
+stats = df.melt(
+    id_vars=["Tipo de dato", "Archivo", "Tamaño (bytes)", "Repetición"],
+    value_vars=["AES-256 tiempo (s)", "ECDSA tiempo (s)"],
+    var_name="Protocolo",
+    value_name="Tiempo (s)"
+)
 
-summary = stats.groupby(["Tipo de dato", "Archivo", "Protocolo"]).agg(
-    media_mean=("Tiempo (s)", "mean"),
-    std_dev=("Tiempo (s)", "std"),
-    median_val=("Tiempo (s)", "median"),
-    var_val=("Tiempo (s)", "var"),
-    min_val=("Tiempo (s)", "min"),
-    max_val=("Tiempo (s)", "max")
-).reset_index()
+grouped = stats.groupby(["Archivo", "Protocolo"])["Tiempo (s)"]
+summary = grouped.agg([
+    ("media", "mean"),
+    ("mediana", "median"),
+    ("Desviación estándar", "std"),
+    ("Varianza", "var"),
+    ("min_time", "min"),
+    ("max_time", "max"),
+])
 
-summary["coef_var"] = summary["std_dev"] / summary["media_mean"]
+summary["coef_variation"] = summary["Desviación estándar"] / summary["media"]
+summary.reset_index(inplace=True)
 
-column = ["Tipo de dato", "Archivo", "max_val", "coef_var"]
-
-filter = summary[column]
-
-filter.to_csv("analisis.csv", index=False)
-
-filter.head()
-
-print(summary)
-
+summary.to_csv("analisis_estadistico.csv", index=False)
